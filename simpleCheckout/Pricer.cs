4a77bad0dd3747e100ccbbbd392e25a7d11269d1
Mike Harper <mike@mcharper.com>
2019-03-02 22:36:52 +0000

    Add basic tests for Pricer

--- simpleCheckout/Pricer.cs
+++ simpleCheckout/Pricer.cs
@@ -1,13 +1,27 @@
 ﻿
+using simpleCheckout.Exceptions;
 using System;
+using System.Text.RegularExpressions;
 
 namespace simpleCheckout
 {
     public class Pricer : IPricer
     {
+        private const string _validItemCode = "^([A-Z])$";
+
         public int GetPrice(char itemCode, int quantity)
         {
-            throw new NotImplementedException();
+            if (!Regex.IsMatch(itemCode.ToString(), _validItemCode))
+            {
+                throw new ItemCodeInvalidException();
+            }
+
+            if (quantity < 1)
+            {
+                throw new QuantityInvalidException();
+            }
+
+            return 0;
         }
     }
 }
---
+++ testSimpleCheckout/TestPricer.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using simpleCheckout;
+using simpleCheckout.Exceptions;
+using System;
+
+namespace testSimpleCheckout
+{
+    [TestClass]
+    public class TestPricer
+    {
+        [TestMethod]
+        [ExpectedException(typeof(ItemCodeInvalidException))]
+        public void GetPriceThrowsExceptionIfItemCodeIsInvalid()
+        {
+            var sut = new Pricer();
+
+            sut.GetPrice(' ', 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(QuantityInvalidException))]
+        public void GetPriceThrowsExceptionIfQuantityIsInvalid()
+        {
+            var sut = new Pricer();
+
+            sut.GetPrice('A', -1);
+        }
+    }
+}
