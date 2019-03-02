929d5f83e001f6fea490ff032416061b59b12623
Mike Harper <mike@mcharper.com>
2019-03-02 23:10:56 +0000

    Add rudimentary and inaccurate (for now) lookup of special offer items

--- simpleCheckout/Pricer.cs
+++ simpleCheckout/Pricer.cs
@@ -20,8 +20,17 @@ namespace simpleCheckout
             { 'D', 15 }
         };
 
+        // TODO - Extract from here
+        private Dictionary<char, Tuple<int,int>> _offerList = new Dictionary<char, Tuple<int, int>>()
+        {
+            { 'A', new Tuple<int, int>(3, 130) },
+            { 'B', new Tuple<int, int>(2, 45) },
+        };
+
         public int GetPrice(char itemCode, int quantity)
         {
+            int price = 0;
+
             if (!Regex.IsMatch(itemCode.ToString(), _validItemCode))
             {
                 throw new ItemCodeInvalidException();
@@ -32,12 +41,21 @@ namespace simpleCheckout
                 throw new QuantityInvalidException();
             }
 
-            if(!_priceList.ContainsKey(itemCode))
+            if (!_priceList.ContainsKey(itemCode))
             {
                 throw new ItemCodeHasNoPriceException();
             }
 
-            return _priceList[itemCode] * quantity;
+            if (_offerList.ContainsKey(itemCode))
+            {
+                price = _offerList[itemCode].Item2;
+            }
+            else
+            {
+                price = _priceList[itemCode] * quantity;
+            }
+
+            return price;
         }
     }
 }
