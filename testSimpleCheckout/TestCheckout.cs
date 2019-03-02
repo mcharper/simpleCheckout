950306c2af116c9859cd7c4e9a0867dfaf46a158
Mike Harper <mike@mcharper.com>
2019-03-02 19:40:46 +0000

    Start adding tests for Checkout class

--- testSimpleCheckout/TestCheckout.cs
+++ testSimpleCheckout/TestCheckout.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace simpleCheckout
 {
@@ -6,8 +7,36 @@ namespace simpleCheckout
     public class TestCheckout
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CheckoutScanAcceptsValidInput()
         {
+            var sut = new Checkout();
+
+            try
+            {
+                sut.Scan("A");
+            }
+            catch(Exception ex)
+            {
+                Assert.Fail("Checkout.Scan should accept valid input");
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        public void CheckoutScanThrowsExceptionForBlankItemCode()
+        {
+            var sut = new Checkout();
+
+            sut.Scan("");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(System.Exception))]
+        public void CheckoutScanThrowsExceptionForTooLongNonKeywordItemCode()
+        {
+            var sut = new Checkout();
+
+            sut.Scan("AAA");
         }
     }
 }
