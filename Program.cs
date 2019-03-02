3683551ea4df05417780b36c2fab270f10f022af
Mike Harper <mike@mcharper.com>
2019-03-02 17:10:07 +0000

    Get started with checkout program as a C# console app.

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace simpleCheckout
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Simple Checkout!");
         }
     }
 }
