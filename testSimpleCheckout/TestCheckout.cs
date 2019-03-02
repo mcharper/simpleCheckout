91b48dc9a7ad261bf7f49e034074cca8d559207a
Mike Harper <mike@mcharper.com>
2019-03-02 19:20:10 +0000

    Create a test project and refactor main project into its own folder.

---
+++ testSimpleCheckout/TestCheckout.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace simpleCheckout
+{
+    [TestClass]
+    public class TestCheckout
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
