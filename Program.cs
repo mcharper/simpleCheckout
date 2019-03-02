bd3348fd0397c6fd984bda62e2e0b56a53ae2d04
Mike Harper <mike@mcharper.com>
2019-03-02 17:23:31 +0000

    Add basic framework for manually simulating the scanning process

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace simpleCheckout
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Simple Checkout!");
+            Console.WriteLine("Welcome to simple Checkout!");
+            Console.WriteLine();
+            Console.WriteLine("Please enter the code for each item followed by return.");
+            Console.WriteLine();
+            Console.WriteLine("To find out the total cost so far enter TOTAL followed by return.");
+            Console.WriteLine("To exit enter DONE followed by return.");
+            Console.WriteLine();
         }
     }
 }
