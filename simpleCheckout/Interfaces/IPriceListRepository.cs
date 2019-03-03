c6e46448cc987ec1b565eca327d665375cf30490
Mike Harper <mike@mcharper.com>
2019-03-03 15:00:40 +0000

    Small refactor

---
+++ simpleCheckout/Interfaces/IPriceListRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace simpleCheckout
+{
+    public interface IPriceListRepository
+    {
+        Dictionary<char, int> GetPriceList();
+    }
+}
